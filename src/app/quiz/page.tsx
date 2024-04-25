c28af936d30aac1111b6ada23961ad16c564a32a
1122c <118850768+1122c@users.noreply.github.com>
2024-04-25 13:39:58 -0700

    edits to quiz > page.tsx

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,13 +1,82 @@
+"use client";
+
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 
+const questions = [
+  {
+    questionText: "What is React?",
+    answers: [
+      {
+        answerText: "a library for building user interfaces",
+        isCorrect: true,
+        id: 1,
+      },
+      { answerText: "a front-end framework", isCorrect: false, id: 2 },
+      { answerText: "a back-end framework", isCorrect: false, id: 3 },
+      { answerText: "a database", isCorrect: false, id: 4 },
+    ],
+  },
+  {
+    questionText: "What is JSX?",
+    answers: [
+      { answerText: "JavaScript XML", isCorrect: true, id: 1 },
+      { answerText: "JavaScript", isCorrect: false, id: 2 },
+      { answerText: "JavaScript and XML", isCorrect: false, id: 3 },
+      { answerText: "JavaScript and HTML", isCorrect: false, id: 4 },
+    ],
+  },
+  {
+    questionText: "What is the virtual DOM?",
+    answers: [
+      {
+        answerText: "A virtual representation of the DOM",
+        isCorrect: true,
+        id: 1,
+      },
+      { answerText: "A real DOM", isCorrect: false, id: 2 },
+      {
+        answerText: "A virtual representation of the browser",
+        isCorrect: false,
+        id: 3,
+      },
+      {
+        answerText: "A virtual representation of the server",
+        isCorrect: false,
+        id: 4,
+      },
+    ],
+  },
+];
+
 export default function Home() {
+  const [started, setStarted] = useState(false);
+
+  const handleNext = () => {
+    setStarted((prev) => !prev); // Toggles the started state for demonstration
+  };
+
   return (
     <div className="flex flex-col flex-1">
       <main className="flex justify-center flex-1">
-        <h1 className="text-3xl font-bold">Welcome to Quiz Time! 👋</h1>
+        {!started ? (
+          <h1 className="text-3xl font-bold">Welcome to Quiz Time! 👋</h1>
+        ) : (
+          <div>
+            <h2 className="text-3xl font-bold">
+              What is your level of understanding React?
+            </h2>
+            <div className="grid grid-cols-1 gap-6 mt-6">
+              <Button variant="secondary">Beginner</Button>
+              <Button variant="secondary">Intermediate</Button>
+              <Button variant="secondary">Advanced</Button>
+              <Button variant="secondary">Godlike</Button>
+            </div>
+          </div>
+        )}
       </main>
       <footer className="footer pb-9 px-6 relative mb-0">
-        <Button>Start</Button>
+        <Button onClick={handleNext}>{!started ? "Start" : "Next"}</Button>
       </footer>
     </div>
   );
