b65c908d968e54be2db3706c3ae0f5192d08a30e
1122c <118850768+1122c@users.noreply.github.com>
2024-05-21 10:31:56 -0700

    db files added, changes to route.ts also

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,6 +6,8 @@ import { HumanMessage } from "@langchain/core/messages";
 import { PDFLoader } from "@langchain/document_loaders/fs/pdf";
 import { JsonOutputFunctionsParser } from "langchain/output_parsers";
 
+import saveQuiz from "./quiz/generate/saveToDb";
+
 export async function POST(req: NextRequest) {
   const body = await req.formData();
   const document = body.get("pdf");
