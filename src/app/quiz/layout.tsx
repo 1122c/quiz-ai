dc3ceda41c53199584c548da521991816551b6fb
1122c <118850768+1122c@users.noreply.github.com>
2024-05-27 10:46:28 -0700

    changes to layout and header files

--- src/app/quiz/layout.tsx
+++ src/app/quiz/layout.tsx
@@ -1,10 +1,16 @@
+import Header from "../../components/ui/header";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="main-layout flex flex-col flex-1 max-w-96 w-full m-auto h-screen gap-6">
-      {children}
-    </div>
+    <>
+      <Header />
+      <div className="main-layout flex flex-col flex-1 max-w-96 w-full m-auto h-screen gap-6">
+        {children}
+      </div>
+    </>
   );
 }
+
 // export default function Layout({ children }: { children: React.ReactNode }) {
 //   return (
 //     <div className="flex flex-col flex-1 max-w-96 w-full m-auto h-screen gap-6">
---
+++ src/components/ui/header.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <Link href={"/quiz"}>Sample Quiz</Link>
+        <Link href={"/quiz/new"}>New Quiz</Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
