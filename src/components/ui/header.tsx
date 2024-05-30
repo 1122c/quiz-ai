b2028d7e5dd8ef9b391cd68fa2f30727c8a66a2f
1122c <118850768+1122c@users.noreply.github.com>
2024-05-30 08:07:46 -0700

    changes to header and layout files

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -3,9 +3,19 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header>
-      <nav>
-        <Link href={"/quiz"}>Sample Quiz</Link>
-        <Link href={"/quiz/new"}>New Quiz</Link>
+      <nav className="px-4 py-2.5 flex gap-2">
+        <Link
+          className="underline"
+          href={"/quiz"}
+        >
+          Sample Quiz
+        </Link>
+        <Link
+          href={"/quiz/new"}
+          className="underline"
+        >
+          New Quiz
+        </Link>
       </nav>
     </header>
   );
