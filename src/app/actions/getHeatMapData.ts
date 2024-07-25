c9da4f4a0e1acf518dffd7a489c909a2fd3b8da7
1122c <118850768+1122c@users.noreply.github.com>
2024-07-25 13:33:30 -0700

    corrected heatmap errors

--- src/app/actions/getHeatMapData.ts
+++ src/app/actions/getHeatMapData.ts
@@ -1,7 +1,6 @@
 import { quizzes, questions, quizSubmissions, users } from "@/db/schema";
 import { auth } from "@/auth";
 import { db } from "@/db";
-import { count, eq, avg } from "drizzle-orm";
 import { count, eq, avg, sql } from "drizzle-orm";
 
 const getHeatMapData = async () => {
@@ -12,14 +11,17 @@ const getHeatMapData = async () => {
     return;
   }
 
-  const data = await db.select({
-    createdAt: quizSubmissions.createdAt,
-    count: sql<number>`cast(count(${quizSubmissions.id}) as int)`,
-  });
-  .from(quizSubmissions)
-  .innerJoin(quizzes, eq(quizSubmissions.quizId, quizzes.id))
-  .innerJoin(users, eq(quizzes.userId, users.id));
-  .groupBy(quizSubmissions.createdAt)
+  const data = await db
+    .select({
+      createdAt: quizSubmissions.createdAt,
+      count: sql<number>`cast(count(${quizSubmissions.id}) as int)`,
+    })
+    .from(quizSubmissions)
+    .innerJoin(quizzes, eq(quizSubmissions.quizId, quizzes.id))
+    .innerJoin(users, eq(quizzes.userId, users.id))
+    .groupBy(quizSubmissions.createdAt);
+
+  return data;
 };
 
 export default getHeatMapData;
