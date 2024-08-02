3d7bac8a776871c15f92691e3f455b3901c81b2a
1122c <118850768+1122c@users.noreply.github.com>
2024-08-02 15:17:30 -0700

    added subscribe btn file

---
+++ src/app/(user)/billing/SubscribeBtn.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { getStripe } from "@/lib/stripe-client";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+
+type Props = {
+  userId?: string;
+  price: string;
+};
+
+const SubscribeBtn = ({ userId, price }: Props) => {
+  const router = useRouter();
+
+  const handleCheckout = async (price: string) => {
+    if (!userId) {
+      router.push("/login");
+      return;
+    }
+
+    try {
+      const { sessionId } = await fetch("/api/stripe/checkout-session", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ price }),
+      }).then((res) => res.json());
+
+      const stripe = await getStripe();
+      stripe?.redirectToCheckout({ sessionId });
+    } catch (error) {
+      console.log("subscribe button error", error);
+    }
+  };
+
+  return (
+    <Button onClick={() => handleCheckout(price)}>Upgrade Your Plan</Button>
+  );
+};
+
+export default SubscribeBtn;
