15812481e4f5182c262c677fef1828482727d02a
1122c <118850768+1122c@users.noreply.github.com>
2024-08-05 08:54:28 -0700

    adding subscription files and logic

--- src/components/ui/NavMenu.tsx
+++ src/components/ui/NavMenu.tsx
@@ -57,8 +57,13 @@ export function NavMenu() {
             <span>Profile</span>
           </DropdownMenuItem>
           <DropdownMenuItem>
-            <CreditCard className="mr-2 h-4 w-4" />
-            <span>Billing</span>
+            <Link
+              href="/billing"
+              className="flex flex-row"
+            >
+              <CreditCard className="mr-2 h-4 w-4" />
+              <span>Billing</span>
+            </Link>
           </DropdownMenuItem>
           <DropdownMenuItem>
             <Settings className="mr-2 h-4 w-4" />
